2e755f2424646677ba5e0f3e64eb3e01e98beade
Henrique Kuwai <henrique.hideki@creditas.com>
2026-07-18 01:01:44 -0300

    first commit

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'pace-timer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalação do Service Worker e salvamento dos arquivos essenciais
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ativação e limpeza de caches antigos
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepta as chamadas de rede para rodar perfeitamente em modo Offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
